87880d9bc5dc51539e04a24f1c1e8a48377f0f91
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-05-27 09:35:07 +0800

    chore: display-noe(TODO)

--- src/App.jsx
+++ src/App.jsx
@@ -60,43 +60,56 @@ const HeightTransition = (props) => {
     'angular'
   ]
   const listRef = React.useRef(null)
+  const _heihgt = React.useRef(0)
   const show = React.useRef(false)
   const [active, setActive] = React.useState(false)
   const [_style, setStyle] = useState({
     height: '0',
-    // display: 'none',
-    overflow: 'hidden'
   })
 
   React.useEffect(() => {
+    _heihgt.current = listRef.current.clientHeight 
     console.log(listRef.current.offsetHeight);
   }, [])
 
+  React.useEffect(() => {
+ 
+  })
+
   const handleClick = () => {
+
     if (show.current) {
       //关闭
       setStyle({
         height: '0',
-        overflow: 'hidden'
       })
 
       show.current = false
     } else {
       //展开
       setStyle({
-        height: listRef.current.offsetHeight + 24 + 'px',
+        height: _heihgt.current + 24 + 'px',
       })
       
       show.current = true
-
     }
+  }
+
+  const handleTransitioned = () => {
+  //  if(show.current) {
+  //   return
+  //  } else {
+  //    setStyle({
+  //      display: 'none',
+  //    })
+  //  }
   }
   return (
     <section className='flex flex-col items-center justify-center mt-16'>
 
 
       <div onClick={handleClick} className="w-64px h-42px bg-blue-400 rounded-xl text-center leading-42px cursor-pointer">切换</div>
-      <div style={_style } className={`list-m `}>
+      <div style={_style } className={`list-m `} onTransitionEnd={handleTransitioned}>
         <div className='w-200px bg-gray-300 text-center' ref={listRef}>
           {
             list.map((val, idx) => (
